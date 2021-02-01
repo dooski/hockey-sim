cffa81e391e850c9b160c49d65960740e9f21ffe
dooski <dmckeon495@gmail.com>
2021-02-01 12:35:12 -0500

    added random game picker; will pick two random teams to play. games run every 18 minutes

--- control.js
+++ control.js
@@ -2,29 +2,25 @@ const data = require("./gens/data.json")
 const sim = require("./gens/sim.js")
 const fs = require("fs")
 
-
-function test(){ 
-    let teams1 = [data.teams.chicago, data.teams.ottawa]
-    let teams2 = [data.teams.vancouver, data.teams.boston]
-    let teams3 = [data.teams.pittsburgh, data.teams.philly]
-    let teams4 = [data.teams.montreal, data.teams.newYork]
-    let teams5 = [data.teams.toronto, data.teams.burlington]
-    let teams6 = [data.teams.buffalo, data.teams.rochester]
-    function g1(){sim.game(teams1, 4800)}
-    function g2(){sim.game(teams2, 4800)}
-    function g3(){sim.game(teams3, 4800)}
-    function g4(){sim.game(teams4, 4800)}
-    function g5(){sim.game(teams5, 4800)}
-    function g6(){sim.game(teams6, 4800)}
-    setTimeout(g1, 100)
-    setTimeout(g2, 900200)
-    setTimeout(g3, 1800300)
-    setTimeout(g4, 2700400)
-    setTimeout(g5, 3600500)
-    setTimeout(g6, 4500600)
+function rng(z) {
+    return Math.floor(Math.random() * z)
 }
 
-setInterval(test, 5501000)
+function test(){ 
+    const league = [data.teams.buffalo, data.teams.boston, data.teams.rochester, data.teams.ottawa, data.teams.toronto, data.teams.montreal, data.teams.newYork, data.teams.philly, data.teams.pittsburgh, data.teams.vancouver, data.teams.chicago, data.teams.burlington]
+    let teams = pickTeams(league)
+    sim.game(teams, 6000) 
+}
+
+function pickTeams(league){
+    let z1 = rng(league.length)
+    let team1 = league[z1]
+    let z2 = rng(league.length)
+    let team2 = league[z2]
+    if (team1 == team2) {pickTeams(league)} else return [team1, team2]
+}
+
+setInterval(test, 1085000)
 
 async function packageReturn(req, res) {
     fs.readFile('./gens/currentGame.json', 'utf8', (err, data) => 
